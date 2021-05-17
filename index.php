e6ab0b5bc4cf007b3fda8be9914fbb22d959f0ba
MarcoBagnasco <bagna.hbk@gmail.com>
2021-05-17 16:13:29 +0200

    add get and print censured string

--- index.php
+++ index.php
@@ -9,13 +9,20 @@
 <body>
     <h1>PHP Badwords</h1>
 
-    
     <?php
     // Variables
-    $string = 'Mentre andavo nelle Ardenne vidi un uomo e sette donne, ogni donna ha sette sacche, ogni sacca sette gatte, ogni gatta sette figli; gattini gatte sacche donne, quanti andavano nelle Ardenne?'
+    $string = 'Mentre andavo nelle Ardenne vidi un uomo e sette donne, ogni donna ha sette sacche, ogni sacca sette gatte, ogni gatta sette figli; gattini gatte sacche donne, quanti andavano nelle Ardenne?';
+    // Data from Query String
+    $badword = $_GET['badword'];
+    // Censured string
+    $censured = str_replace($badword, '***', $string);
     ?>
 
+    <!-- Original String -->
     <p><?php echo $string?></p>
+    <!-- String Length -->
     <p>The paragraph length is: <?php echo strlen($string) ?></p>
+    <!-- Censured String -->
+    <p><?php echo $censured ?></p>
 </body>
 </html>
