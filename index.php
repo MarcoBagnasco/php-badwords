db6c480f8c25eb3a891d63a8d0c599e0686b5c07
MarcoBagnasco <bagna.hbk@gmail.com>
2021-05-17 15:55:36 +0200

    create variable and print on screen

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
     <title>PHP Badwords</title>
 </head>
 <body>
+    <h1>PHP Badwords</h1>
+
     
+    <?php
+    // Variables
+    $string = 'Mentre andavo nelle Ardenne vidi un uomo e sette donne, ogni donna ha sette sacche, ogni sacca sette gatte, ogni gatta sette figli; gattini gatte sacche donne, quanti andavano nelle Ardenne?'
+    ?>
+
+    <p><?php echo $string?></p>
 </body>
 </html>
